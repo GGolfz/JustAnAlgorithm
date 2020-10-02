acb51c74f96746ca60f6b53b947a6cf8a54dd8a4
thanawat.bcr <thanwat.bcr@gmail.com>
2020-10-02 19:04:55 +0700

    [ADD] linear factorial by Tutor

---
+++ Factorial/Linear/Factorial.java
@@ -0,0 +1,18 @@
+
+/**
+ * Factorial
+ */
+import java.util.Scanner;
+
+public class Factorial {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int ans = 1;
+        for (int i = 2; i <= n; i++) {
+            ans *= i;
+        }
+        System.out.println(ans);
+    }
+}
