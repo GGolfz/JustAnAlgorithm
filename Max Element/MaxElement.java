bbe5b4236234e9ea28f0deb79dbb548183f702e1
note <note16651@hotmail.co.th>
2020-10-02 23:15:36 +0700

    ADD[MaxElement] FIX

--- Max Element/MaxElement.java
+++ Max Element/MaxElement.java
@@ -1,10 +1,9 @@
 public class MaxElement{
     public static void main(String[]args){
         int[] test = {10,7,23,56,82190,123,642,34,72,8934,20043,43,1,-1,43};
-        int[]test2 ={-1,-2,-3};
-        int max= test2[0];
-        for(int i=0;i<test2.length;i++){
-            if(test2[i] > max){
+        int max= test[0];
+        for(int i=0;i<test.length;i++){
+            if(test[i] > max){
                 max = test[i];
             }
         }
